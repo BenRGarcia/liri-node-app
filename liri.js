941aa23d31131cb017f0021d161b66a300df5beb
Ben Garcia <ben@seebenprogram.com>
2018-03-16 23:05:17 -0400

    Add comment of npm package links

--- liri.js
+++ liri.js
@@ -2,3 +2,10 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+/*  npm packages:
+ *    https://www.npmjs.com/package/twitter
+ *    https://www.npmjs.com/package/node-spotify-api
+ *    https://www.npmjs.com/package/request
+ *    https://www.npmjs.com/package/dotenv
+ */
