4660638a037b9ccbc049ba8373262ec198173037
Ben Garcia <ben@seebenprogram.com>
2018-03-18 16:04:32 -0400

    Add Spotify song feature

--- liri.js
+++ liri.js
@@ -20,16 +20,12 @@ var client = new Twitter(keys.twitter);
 const command = process.argv[2];
 const param = process.argv[3];
 
-// For debugging
-// console.log(`Command is: ${command}`);
-// console.log(`Param is: ${param}`);
-
 /*  Twitter | $ node liri.js my-tweets
  *  Docs: https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
  */
 if (command === 'my-tweets') {
   // Establish query parameters
-  var params = {
+  let params = {
     screen_name: 'SeeBenProgram',
     count: 20
   };
@@ -47,11 +43,44 @@ if (command === 'my-tweets') {
 }
 
 /*  Spotify | $ node liri.js spotify-this-song '<song name here>'
- *  Docs: 
- *  
+ *  Docs: https://beta.developer.spotify.com/documentation/web-api/
  */
 if (command === 'spotify-this-song') {
-  let songName = param || 'The Sign';
+  // If user provided song name
+  if (param) {
+    spotify.search({
+      type: 'track',
+      query: param,
+      limit: 1
+    }, function (error, data) {
+      if (!error) {
+        // Log song details
+        console.log(`\n Artist name: ${data.tracks.items[0].artists[0].name}`);
+        console.log(`  Album name: ${data.tracks.items[0].album.name}`);
+        console.log(`   Song name: ${data.tracks.items[0].name}`);
+        console.log(`Preview link: ${data.tracks.items[0].preview_url || '(not available)'}`);
+      } else {
+        console.log(error);
+      }
+    });
+  } 
+  // If user did not provide song name
+  else {
+    // Default if no 'param': Ace of Base 'The Sign'
+    let queryURL = 'https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc';
+    spotify
+    .request(queryURL)
+    .then(function(data){
+      // Log song details
+      console.log(`\n Artist name: ${data.artists[0].name}`);
+      console.log(`  Album name: ${data.album.name}`);
+      console.log(`   Song name: ${data.name}`);
+      console.log(`Preview link: ${data.preview_url}`);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
 }
 
 // OMDB - 'movie-this' | node liri.js movie-this '<movie name here>'
