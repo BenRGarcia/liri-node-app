a369a6e70396a1b2eb427f5b2feb1851a4f78174
Ben Garcia <ben@seebenprogram.com>
2018-03-18 10:46:22 -0400

    Import packages, initialize

--- liri.js
+++ liri.js
@@ -1,7 +1,20 @@
+// Import env variables, packages
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 var keys = require("./keys.js");
+
+// Initialize packages with env var config
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+// Declare variables for arguments
+const command = process.argv[2];
+const param = process.argv[3];
+
+// For debugging
+console.log(`Command is: ${command}`);
+console.log(`Param is: ${param}`);
 
 /*  npm packages:
  *    https://www.npmjs.com/package/twitter
