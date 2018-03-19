ab028fd0f3d42daa8d4fe45566eb19394080682c
Ben Garcia <ben@seebenprogram.com>
2018-03-18 20:26:52 -0400

    Delete console logs for debugging

--- liri.js
+++ liri.js
@@ -24,6 +24,7 @@ let param = process.argv[3];
 // Call function to route command
 routeCommand(command, param);
 
+// A function to route command based on its value
 function routeCommand(command, param) {
   switch (command) {
     case 'my-tweets':
@@ -48,16 +49,13 @@ function routeCommand(command, param) {
  *  Docs: https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
  */
 function searchTwitter() {
-  // Establish query parameters
   let params = {
     screen_name: 'SeeBenProgram',
     count: 20
   };
-  // Make GET request based on parameters
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
       for (let tweet of tweets) {
-        // Log tweets
         console.log(`\nOn ${tweet.created_at}, ${tweet.user.screen_name} tweeted:\n'${tweet.text}'`);
       }
     } else {
@@ -70,7 +68,6 @@ function searchTwitter() {
  *  Docs: https://beta.developer.spotify.com/documentation/web-api/
  */
 function searchSpotify(param) {
-  // If user provided song name
   if (param) {
     spotify.search({
       type: 'track',
@@ -78,7 +75,6 @@ function searchSpotify(param) {
       limit: 1
     }, function (error, data) {
       if (!error) {
-        // Log song details
         console.log(`\n Artist name: ${data.tracks.items[0].artists[0].name}`);
         console.log(`  Album name: ${data.tracks.items[0].album.name}`);
         console.log(`   Song name: ${data.tracks.items[0].name}`);
@@ -87,15 +83,11 @@ function searchSpotify(param) {
         console.log(error);
       }
     });
-  }
-  // If user did not provide song name
-  else {
-    // Default if no 'param': Ace of Base 'The Sign'
+  } else {
     let queryURL = 'https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc';
     spotify
       .request(queryURL)
       .then(function (data) {
-        // Log song details
         console.log(`\n Artist name: ${data.artists[0].name}`);
         console.log(`  Album name: ${data.album.name}`);
         console.log(`   Song name: ${data.name}`);
@@ -111,14 +103,11 @@ function searchSpotify(param) {
  *  Docs: http://www.omdbapi.com/
  */
 function searchOMBD(param) {
-  // Sanitize Movie Title, add fallback if no movie name provided
   let movieName = param || 'Mr. Nobody';
   let queryName = movieName.replace(' ', '+');
   let queryURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movieName;
-
   request(queryURL, function (error, response, body) {
     if (!error) {
-      // console.log(JSON.parse(body));
       let movie = JSON.parse(body);
       console.log(`\n              Movie Title: ${movie.Title}`);
       console.log(`                     Year: ${movie.Year}`);
