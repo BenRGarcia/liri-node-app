b977a8493419005db33885ba7576367b7c7953e5
Ben Garcia <ben@seebenprogram.com>
2018-03-19 13:38:26 -0400

    Modularize ParseCommand

---
+++ parseCommand.js
@@ -0,0 +1,19 @@
+// ParseCommand constructor
+const ParseCommand = function() {};
+
+// Returns 'command' & 'param' variables object from given 'fileName'
+ParseCommand.prototype.parse = function(fileName) {
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    // Error handling
+    if (err) throw err;
+    // Parse file
+    let dataArray = data.split(',');
+    // Declare variables for data
+    command = array[0];
+    param = array[1];
+    // Return object of command & param
+    return { command, param };
+  });
+}
+
+module.exports = ParseCommand;
