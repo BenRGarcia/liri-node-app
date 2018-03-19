20af2571dbb09c7a94efa7f9d316d7cbe904d3b0
Ben Garcia <ben@seebenprogram.com>
2018-03-19 13:11:42 -0400

    Begin modularization

--- liri.js
+++ liri.js
@@ -1,23 +1,25 @@
-/*  npm packages:
- *    https://www.npmjs.com/package/twitter
- *    https://www.npmjs.com/package/node-spotify-api
- *    https://www.npmjs.com/package/request
- *    https://www.npmjs.com/package/dotenv
- */
-
-// Import env variables, packages
+// Environment variables
 require("dotenv").config();
-var Spotify = require('node-spotify-api');
-var Twitter = require('twitter');
-var keys = require("./keys.js");
-var request = require('request');
-var fs = require('fs');
+const keys = require("./keys.js");
+// Twitter
+const Twitter = require('twitter');
+const twitter = new Twitter(keys.twitter);
+const SearchTwitter = require('./searchTwitter');
+const searchTwitter = new SearchTwitter();
+// Spotify
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+const SearchSpotify = require('./searchSpotify');
+const searchSpotify = new SearchSpotify();
+// OMDB
+const request = require('request');
+const SearchOMDB = require('./searchOMDB');
+// Data logging
+const fs = require('fs');
+const DataLogger = require('./dataLogger');
+const dataLogger = new DataLogger();
 
-// Initialize packages with env var configs
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-
-// Declare variables for arguments
+// Declare variables for arguments passed
 let command = process.argv[2];
 let param = process.argv[3];
 
@@ -28,7 +30,12 @@ routeCommand(command, param);
 function routeCommand(command, param) {
   switch (command) {
     case 'my-tweets':
-      searchTwitter();
+      // Define (hard coded) search parameters
+      let paramObj = { screen_name: 'SeeBenProgram', count: 20 };
+      // Execute GET request
+      let results = searchTwitter(paramObj);
+      // Log results
+      dataLogger.log(results);
       break;
     case 'spotify-this-song':
       searchSpotify(param);
@@ -37,7 +44,7 @@ function routeCommand(command, param) {
       searchOMBD(param);
       break;
     case 'do-what-it-says':
-      evaluateCommand(command);
+      parseCommand(command);
       break;
     default:
       console.log(`Invalid command: '${command}'`);
@@ -57,7 +64,7 @@ function searchTwitter() {
     if (!err) {
       for (let tweet of tweets) {
         console.log(`\nOn ${tweet.created_at}, ${tweet.user.screen_name} tweeted:\n'${tweet.text}'`);
-        logData(`\nOn ${tweet.created_at}, ${tweet.user.screen_name} tweeted:\n'${tweet.text}'\n`);
+        logToFile(`\nOn ${tweet.created_at}, ${tweet.user.screen_name} tweeted:\n'${tweet.text}'\n`);
       }
     } else {
       console.log(err);
@@ -81,7 +88,7 @@ function searchSpotify(param) {
         console.log(`  Album name: ${track.album.name}`);
         console.log(`   Song name: ${track.name}`);
         console.log(`Preview link: ${track.preview_url || '(not available)'}`);
-        logData(`\n Artist name: ${track.artists[0].name}\n  Album name: ${track.album.name}\n   Song name: ${track.name}\nPreview link: ${track.preview_url || '(not available)'}\n`);
+        logToFile(`\n Artist name: ${track.artists[0].name}\n  Album name: ${track.album.name}\n   Song name: ${track.name}\nPreview link: ${track.preview_url || '(not available)'}\n`);
       } else {
         console.log(err);
       }
@@ -95,7 +102,7 @@ function searchSpotify(param) {
         console.log(`  Album name: ${data.album.name}`);
         console.log(`   Song name: ${data.name}`);
         console.log(`Preview link: ${data.preview_url}`);
-        logData(`\n Artist name: ${data.artists[0].name}\n  Album name: ${data.album.name}\n   Song name: ${data.name}\nPreview link: ${data.preview_url}\n`);
+        logToFile(`\n Artist name: ${data.artists[0].name}\n  Album name: ${data.album.name}\n   Song name: ${data.name}\nPreview link: ${data.preview_url}\n`);
       })
       .catch(function (err) {
         console.log(err);
@@ -121,7 +128,7 @@ function searchOMBD(param) {
       console.log(`                 Language: ${movie.Language}`);
       console.log(`                   Actors: ${movie.Actors}`);
       console.log(`                     Plot: ${movie.Plot}`);
-      logData(`\n              Movie Title: ${movie.Title}\n                     Year: ${movie.Year}\n              IMDB Rating: ${movie.Ratings[0].Value}\n   Rotten Tomatoes Rating: ${movie.Ratings[1].Value}\nCountry in which Produced: ${movie.Country}\n                 Language: ${movie.Language}\n                   Actors: ${movie.Actors}\n                     Plot: ${movie.Plot}\n`);
+      logToFile(`\n              Movie Title: ${movie.Title}\n                     Year: ${movie.Year}\n              IMDB Rating: ${movie.Ratings[0].Value}\n   Rotten Tomatoes Rating: ${movie.Ratings[1].Value}\nCountry in which Produced: ${movie.Country}\n                 Language: ${movie.Language}\n                   Actors: ${movie.Actors}\n                     Plot: ${movie.Plot}\n`);
     } else {
       console.log(err);
     }
@@ -133,18 +140,13 @@ function searchOMBD(param) {
  *        https://nodejs.org/dist/latest-v9.x/docs/api/fs.html#fs_fs_readfile_path_options_callback
  *        https://nodejs.org/dist/latest-v9.x/docs/api/fs.html#fs_fs_appendfile_file_data_options_callback
  */
-function evaluateCommand() {
+function parseCommand() {
   fs.readFile('./random.txt', 'utf8', (err, data) => {
     if (err) throw err;
+    // Parse file, route command
     let array = data.split(',');
     command = array[0];
     param = array[1];
     routeCommand(command, param);
-  });
-}
-
-function logData(data) {
-  fs.appendFile('log.txt', data, 'utf8', err => {
-    if (err) throw err;
   });
 }
