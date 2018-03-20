5701eec9ece6bb9ef9cfd3e50e8194655a243363
Ben Garcia <ben@seebenprogram.com>
2018-03-20 09:45:16 -0400

    return Promise from .search() method

--- custom_modules/searchTwitter.js
+++ custom_modules/searchTwitter.js
@@ -10,26 +10,30 @@ var SearchTwitter = function() {};
 
 // Returns object of tweets
 SearchTwitter.prototype.search = function (paramsObj) {
-  console.log(`.search called with params Obj:`);
-  console.log(paramsObj);
   // Declare path of Twitter API endpoint to query
   let path = 'statuses/user_timeline';
   // Create empty object to store tweets
   let results = {};
   // Make API call, return results object
-  return twitter.get(path, paramsObj, (err, tweets, response) => {
-    // Error handling
-    if (err) throw err;
-    // Iterate over tweets
-    for (let tweet of tweets) {
-      // Declare variables for parsed data
-      let date = tweet.created_at;
-      let name = tweet.user.screen_name;
-      let text = tweet.text;
-      // Add property to results object, date as key
-      results[date] = { date, name, text };
-    }
-    return results;
+  return new Promise ((resolve, reject) => {
+
+    twitter.get(path, paramsObj, (err, tweets, response) => {
+      console.log(`Results from inside SearchTwitter.search() received`);
+      // Error handling
+      if (err) throw err;
+      // Iterate over tweets
+      for (let tweet of tweets) {
+        // Declare variables for parsed data
+        let date = tweet.created_at;
+        let name = tweet.user.screen_name;
+        let text = tweet.text;
+        // Add property to results object, date as key
+        results[date] = { date, name, text };
+      }
+      console.log(results);
+      resolve(results);
+    });
+    
   });
 }
 
